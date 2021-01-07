5076332a7a8919eec28cec1722fb928f19fa4d80
Eyal <maestroeyal@gmail.com>
2021-01-07 07:19:09 +0200

    Add cyclical asteroids
    - Added the Cyclical behaviours to asteroids.
    - Created a duplicate of existing ones in the scene to add as mirrors of the newly cyclical ones.

---
+++ Assets/Scripts/Controller/Floatable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floatable : MonoBehaviour
+{
+    [SerializeField]
+    private bool randomDirection;
+    [SerializeField]
+    private float floatSpeed;
+    [SerializeField]
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (randomDirection)
+        {
+            direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime;
+    }
+}
